1fda1e970eda23454695c142d6ab4e25a4a78c2e
Cameron Grant <cameron@catchdesign.co.nz>
2018-10-30 09:54:18 +1300

    refactor the state out of the quotes

--- src/components/quote-slide-show.js
+++ src/components/quote-slide-show.js
@@ -1,61 +1,44 @@
 import React from "react";
-import { Button } from "reactstrap";
+//import { Button } from "reactstrap";
 class QuoteSlideShow extends React.Component {
-  
+  timerID = 0;
   state = {
-    quotes: []
+    currentQuote: 0
   };
-  
-  static getDerivedStateFromProps({ quotes }) {
-    quotes = quotes.reduce(function(carey, quote, currentIndex) {
-      const index = currentIndex + 1;
-      const current = index === 1 ? true : false;
-      carey.push({ quote, index, current });
-      return carey;
-    }, []);
-    return { quotes };
+
+  componentDidMount() {
+
+    const thirtySeconds = 1000 * 30;
+    this.timerID = window.setInterval(
+      () => this.eventNextSlide(),
+      thirtySeconds
+    );
   }
   
-  // componentDidMount = () => {
-  //   const thirtySeconds = 1000 * 30;
-  //   this.timerID = window.setInterval(
-  //     () => this.eventNextSlide(),
-  //     thirtySeconds
-  //   );
-  // }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
   
-  // componentWillUnmount = () => {
-  //   clearInterval(this.timerID);
-  // }
-  
-  currentQuoteObject = () => this.state.quotes.find(o => o.current);
+  currentQuote = () =>  this.props.quotes[this.state.currentQuote];
 
   eventNextSlide = () => {
-    console.log('STATE ' ,this.state.quotes);
-    let currentIndex = this.currentQuoteObject().index;
-
-    let nextObj = this.state.quotes.find(o => o.index === currentIndex + 1);
-    let nextIndex = nextObj ? nextObj.index : 1;
-    let quotes = this.state.quotes.map(function(quoteObj) {
-      quoteObj.current = false;
-      if (quoteObj.index === nextIndex) {
-        quoteObj.current = true;
-      }
-      return quoteObj;
-    });
-    console.log('QUOTES ', quotes);
-    this.setState((state) => {quotes: [...state.quotes, quotes]} );
-   
+    const quoteLength = this.props.quotes.length;
+    let nextQuote = this.state.currentQuote + 1;
+    if(nextQuote >= quoteLength) {
+      nextQuote = 0;
+    }
+    this.setState({currentQuote:nextQuote});
   };
 
   render() {
     return (
       <div className="container mt-5 mb-5">
-        <h1 className="text-center"> {this.state.quotes.find(o => o.current).quote} </h1>
-
+        <h1 className="text-center"> {this.currentQuote()} </h1>
+        {/*
         <Button onClick={this.eventNextSlide} color="primary" size="lg">
           Next Slide {this.state.currentSlide}
         </Button>
+        */}
       </div>
     );
   }
